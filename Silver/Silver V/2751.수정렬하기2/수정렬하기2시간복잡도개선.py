6c0949b8ee87c3c5c7027497dcc6d53caf9879a2
TaeJun Kwon <ktjktj0629@naver.com>
2025-06-30 21:08:22 +0900

    Update 수정렬하기2시간복잡도개선.py

--- Silver/Silver V/2751.수정렬하기2/수정렬하기2시간복잡도개선.py
+++ Silver/Silver V/2751.수정렬하기2/수정렬하기2시간복잡도개선.py
@@ -5,5 +5,8 @@ n = int(sys.stdin.readline().strip())
 lst = [int(sys.stdin.readline().strip()) for i in range(n)]
 # 정렬 
 lst.sort()
+'''
+ 리스트 lst에 있는 정수들을 문자열로 바꾸고, 개행 문자 \n으로 연결한 후, 한 번에 출력
+'''
 # 효율적으로 값을 출력 (print보다 훨씬 빠름) 
 sys.stdout.write('\n'.join(map(str, lst))+'\n')
