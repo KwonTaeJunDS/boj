e4b8e1bd321d54f10e0e057c106063b8dda994df
TaeJun Kwon <ktjktj0629@naver.com>
2026-02-03 21:18:19 +0900

    Update 나무자르기1.py

--- Silver/Silver II/*나무 자르기(이진탐색대표문제)/나무자르기1.py
+++ Silver/Silver II/*나무 자르기(이진탐색대표문제)/나무자르기1.py
@@ -18,15 +18,16 @@ def bin_search(arr, target, start, end):
                 ans += (tree - mid)
 
         
-        # 자른 나무가 target보다 작다면 end를 줄임 (이래야 target보다 커짐 (빼기니깐))
+        # 자른 나무가 target보다 작다면 end를 줄임 (이래야 target보다 커짐 (빼니깐))
         if ans < target:
             end = mid - 1
         
-        # 자른 나무가 target보다 크다면 start를 키움 (더 빼야 줄어듦)
+        # 자른 나무가 target보다 크다면 start를 키움 (더 빼야 줄어듦) 
         elif ans >= target:
             start = mid + 1
 
     return end
+
 
 
 
