410868660bfc4aa5bcc5780dd4a5b3fb3eb3fbd5
TaeJun Kwon <ktjktj0629@naver.com>
2026-03-06 14:02:16 +0900

    Create 2.py

---
+++ Silver/Silver IV/설탕배달/2.py
@@ -0,0 +1,20 @@
+import sys
+
+
+num = int(sys.stdin.readline().strip())
+
+ans = 0
+
+while num >= 0:
+    if num % 5 == 0:
+        ans += num // 5
+        num = 0
+        break
+    else:
+        num -= 3
+        ans += 1
+    
+if num != 0:
+    print(-1)
+else:
+    print(ans)  
