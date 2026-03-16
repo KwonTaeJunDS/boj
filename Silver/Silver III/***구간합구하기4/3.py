cb574388e9c7d55ef04dbbb2aa62930538521761
TaeJun Kwon <ktjktj0629@naver.com>
2026-03-16 19:49:38 +0900

    Create 3.py

---
+++ Silver/Silver III/***구간합구하기4/3.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+lst = list(map(int, input().split()))
+
+ans = [0]
+temp = 0
+
+for val in lst:
+    temp += val
+    ans.append(temp)
+
+
+
+for c in range(m):
+    i, j = map(int, input().split())
+
+    print(ans[j] - ans[i-1])
+
+    
