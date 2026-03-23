db8d2c40bcd62b4f96c149f867e60abe9204911d
TaeJun Kwon <ktjktj0629@naver.com>
2026-03-23 16:00:51 +0900

    Create 4.py

---
+++ Gold/IV/*** 1253.좋은 수 구하기/4.py
@@ -0,0 +1,35 @@
+import sys
+
+input = sys.stdin.readline
+
+
+num = int(input())
+
+lst = list(map(int, input().split()))
+lst.sort()
+
+ans = 0
+
+for c in range(num):
+
+    find = lst[c]
+    i = 0
+    j = num - 1
+
+
+    while i < j:
+        if lst[i] + lst[j] == find:
+            if i!=c and j!=c:
+                ans += 1
+                break
+            elif i == c:
+                i +=1
+            elif j == c:
+                j -= 1
+
+        elif lst[i] +lst[j] < find:
+            i += 1
+        else:
+            j -= 1
+
+print(ans)
